618f7631f394f0b61da9c6426931a07518f56014
Dzmitry <dimastik98.ru@mail.ru>
2023-10-29 01:34:53 +0300

    Добавление файлов практики

---
+++ pract2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//М = 1; N = 15 -> 120
+//М = 4; N = 8. -> 30
+
+int m = InputNumbers("Введите первое натурально число M: ");
+int n = InputNumbers("Введите второе натуральное число N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSumm(m, n, temp = 0);
+
+void PrintSumm(int m, int n, int summ)
+{
+    
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {summ} ");
+        return;
+    }
+    PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
